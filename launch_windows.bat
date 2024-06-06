97a2dec1667cb5fdaca3ebf16ab9124bbd9c05fb
JoseMiguelFrade <72606196+JoseMiguelFrade@users.noreply.github.com>
2024-06-06 21:44:49 +0100

    updated launch_windows.bat

--- launch_windows.bat
+++ launch_windows.bat
@@ -1,6 +1,7 @@
 @echo off
 REM Activate the virtual environment
-call venv\Scripts\activate
+
+call backend\venv\Scripts\activate
 
 REM Start the backend server
 echo Starting backend server...
@@ -8,8 +9,8 @@ start /B cmd /c "cd backend && flask run"
 
 REM Start the frontend server
 echo Starting frontend server...
+
 cd frontend
 npm run dev
 
 REM Notify the user
-echo Frontend server started. Visit http://localhost:3000 to view the application.
